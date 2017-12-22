9c29d886d6a62533398371b2faaa41e73a99d11e
EzequielValencia <ezequielvalencia1234@gmail.com>
2017-12-22 02:14:48 -0600

    Initial commit

---
+++ src/main/java/com/example/demo/SistemaPuntoDeVentaApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPuntoDeVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPuntoDeVentaApplication.class, args);
+	}
+}
