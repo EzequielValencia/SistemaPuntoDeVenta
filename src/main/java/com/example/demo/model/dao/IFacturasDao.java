a1ac491dae4f55f18831870b41d99a94f836b08b
EzequielValencia <ezequielvalencia1234@gmail.com>
2018-01-31 21:36:31 -0600

    Ultima actualizacion

--- src/main/java/com/example/demo/model/dao/IFacturasDao.java
+++ src/main/java/com/example/demo/model/dao/IFacturasDao.java
@@ -3,7 +3,6 @@ package com.example.demo.model.dao;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.example.demo.model.entity.Factura;
